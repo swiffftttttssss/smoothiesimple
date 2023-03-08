04eadefac444bc5a0cbc0939c660bc7a70a89f04
swiffftttttssss <99203119+swiffftttttssss@users.noreply.github.com>
2023-03-08 06:18:07 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from PIL import Image, ImageTk
 import tkinter.messagebox as messagebox
 import tkinter as tk
 
-
-# Define smoothie and ingredient prices
-smoothie_prices = {"Strawberry Banana": 4.99, "Mango Pineapple": 5.99, "Blueberry Blast": 6.99}
-ingredient_prices = {"Strawberries": 1.99, "Bananas": 0.99, "Mango": 2.99, "Pineapple": 1.49, "Blueberries": 3.99}
+# Define smoothie and fruitcup prices
+smoothie_prices = {"Strawberry Banana": 4.99, "Mango Pineapple": 5.99, "Blueberry Blast": 6.99, "Simply Peach": 6.00}
+fruitcup_prices = {"Strawberries": 1.99, "Bananas": 0.99, "Mango": 2.99, "Pineapple": 1.49, "Blueberries": 3.99}
 
 # Define global variable for total price label
 total_price_label = None
@@ -16,8 +15,8 @@ def add_to_cart(item):
     # Get the price of the item
     if item in smoothie_prices:
         price = smoothie_prices[item]
-    elif item in ingredient_prices:
-        price = ingredient_prices[item]
+    elif item in fruitcup_prices:
+        price = fruitcup_prices[item]
     else:
         price = 0
 
@@ -51,17 +50,24 @@ def place_order():
     if cart_listbox.size() == 0:
         messagebox.showerror("Error", "Your cart is empty. Please add items before placing an order.")
         return
-    # Create a new window
-    order_window = tk.Toplevel(root)
-    order_window.title("Order Placed")
+    
 
-    # Display items ordered
-    order_label = tk.Label(order_window, text="Items ordered:")
-    order_label.pack(padx=20, pady=20)
+    # Create the 2nd window
+    order_window = tk.Toplevel(root)
+    order_window.title("Order Confirmation")
+
+    # Create a frame to hold the text labels
+    frame = tk.Frame(order_window, borderwidth=2, relief="groove")
+    frame.pack(padx=20, pady=20)
+
+    # Display items ordered with a frame around it
+    order_label = tk.Label(frame, text="Items ordered:")
+    order_label.pack(padx=10, pady=10)
 
     for item in cart_listbox.get(0, tk.END):
-        item_label = tk.Label(order_window, text=item)
-        item_label.pack()
+        item_label = tk.Label(frame, text=item)
+        item_label.pack(padx=10, pady=5)
+
 
     # Calculate the total price of the items in the cart
     total_price = sum(float(item.split(" - $")[1]) for item in cart_listbox.get(0, tk.END))
@@ -73,6 +79,8 @@ def place_order():
     # Display a message
     message_label = tk.Label(order_window, text="Your order of has been placed!")
     message_label.pack(padx=20, pady=20)
+
+
 # Create the main window
 root = tk.Tk()
 
@@ -82,12 +90,24 @@ root.configure(background='#ADD8E6')
 # Set window title
 root.title("SimpleSmoothies")
 
-# Load top banner image and resize
-TopBannerImageObject = Image.open("images/smoothiesbanner.jpg").resize((1200,130))
-TopBannerImageObject = ImageTk.PhotoImage(TopBannerImageObject)
+# Load the banner and logo images and resize the banner
+banner_image = Image.open("images/smoothiesbanner.jpg").resize((1200, 130))
+logo_image = Image.open("images/logo.png").resize((100, 100))
 
-# Create label for top banner
-banner_label = tk.Label(root, image=TopBannerImageObject)
+# Create a new image with the desired dimensions
+banner_frame = Image.new("RGB", (1300, 130), color="white")
+
+# Paste the logo image to the left of the banner frame
+banner_frame.paste(logo_image, (0, 15))
+
+# Paste the banner image to the right of the logo
+banner_frame.paste(banner_image, (100, 0))
+
+# Convert the banner frame to a Tkinter PhotoImage object
+banner_photo = ImageTk.PhotoImage(banner_frame)
+
+# Create a label for the banner and display it
+banner_label = tk.Label(root, image=banner_photo)
 banner_label.grid(row=0, column=0, columnspan=3, sticky="nsew")
 
 # Create label for smoothie menu
@@ -95,11 +115,11 @@ smoothie_label = tk.Label(root, text="Smoothie Menu", font=("Arial", 16), backgr
 smoothie_label.grid(row=1, column=0, padx=20, pady=20)
 
 # Create listbox for smoothies
-smoothie_listbox = tk.Listbox(root, height=10, width=50, highlightthickness=4,highlightcolor="black",borderwidth=5)
+smoothie_listbox = tk.Listbox(root, height=10, width=50, highlightthickness=4,highlightcolor="black",borderwidth=5, font=("Arial 12"))
 smoothie_listbox.grid(row=2, column=0, padx=20)
 
 # Add smoothies to listbox
-smoothies = ["Strawberry Banana", "Mango Pineapple", "Blueberry Blast"]
+smoothies = ["Strawberry Banana", "Mango Pineapple", "Blueberry Blast","Simply Peach" ]
 for smoothie in smoothies:
     smoothie_listbox.insert(tk.END, smoothie)
 
@@ -116,6 +136,14 @@ def add_selected_smoothie_to_cart():
         # Call the add_to_cart function with the selected item as an argument
         add_to_cart(selected_item)
 
+#Define a function that changes the background color of the smoothie_add_button to light green when the mouse pointer enters the button
+def on_enter_smoothie_button(event):
+    smoothie_add_button.config(bg="lightgreen")
+
+#Define a function that changes the background color of the smoothie_add_button to white when the mouse pointer leaves the button
+def on_leave_smoothie_button(event):
+    smoothie_add_button.config(bg="white")
+
 # Create the "Add to Cart" button for smoothies
 smoothie_add_button = tk.Button(
     root,
@@ -126,14 +154,20 @@ smoothie_add_button = tk.Button(
     command=add_selected_smoothie_to_cart
 )
 
+# Bind the "<Enter>" and "<Leave>" events to the button
+smoothie_add_button.bind("<Enter>", on_enter_smoothie_button)
+smoothie_add_button.bind("<Leave>", on_leave_smoothie_button)
+
 # Add the button to the grid
-smoothie_add_button.grid(row=3, column=0, padx=20, pady=20)
+smoothie_add_button.grid(row=3, column=0, padx=20, pady=10)
+
+
 # Create label for fruit cup menu
 fruitcup_label = tk.Label(root, text="Fruit Cup Menu", font= ("Arial", 16,), background="#ADD8E6")
 fruitcup_label.grid(row=4, column=0, padx=20, pady=20)
 
 # Create listbox for fruitcups
-fruitcup_listbox = tk.Listbox(root, height=10, width=50, highlightthickness=4,highlightcolor="black",borderwidth=5)
+fruitcup_listbox = tk.Listbox(root, height=5, width=50, highlightthickness=4,highlightcolor="black",borderwidth=5, font=("Arial 12"))
 fruitcup_listbox.grid(row=5, column=0, padx=20)
 
 # Add Fruitcups to listbox
@@ -153,8 +187,16 @@ def add_selected_fruitcup_to_cart():
         # Call the add_to_cart function with the selected item as an argument
         add_to_cart(selected_item)
 
-# Create the "Add to Cart" button for fruit cups
-ingredient_add_button = tk.Button(
+#Define a function that changes the background color of the fruitcup_add_button to light green when the mouse pointer enters the button
+def on_enter_fruitcup_button(event):
+   fruitcup_add_button.config(bg="lightgreen")
+
+#Define a function that changes the background color of the fruitcup_add_button to white when the mouse pointer leaves the button
+def on_leave_fruitcup_button(event):
+    fruitcup_add_button.config(bg="white")
+
+# Create the "Add to Cart" button for smoothies
+fruitcup_add_button = tk.Button(
     root,
     text="Add to Cart",
     bg="white",
@@ -163,12 +205,24 @@ ingredient_add_button = tk.Button(
     command=add_selected_fruitcup_to_cart
 )
 
+# Bind the "<Enter>" and "<Leave>" events to the button
+fruitcup_add_button.bind("<Enter>", on_enter_fruitcup_button)
+fruitcup_add_button.bind("<Leave>", on_leave_fruitcup_button)
+
 # Add the button to the grid
-ingredient_add_button.grid(row=6, column=0, padx=20, pady=20)
+fruitcup_add_button.grid(row=6, column=0, padx=20, pady=20)
 
 # Create label for cart
 cart_label = tk.Label(root, text="Cart", font=("Arial", 16),background="#ADD8E6")
 cart_label.grid(row=1, column=1, padx=20, pady=20)
+
+# Open the image and resize it
+photo_image = Image.open("images/photpeaphoto.jpg").resize((600, 200))
+photo_image = ImageTk.PhotoImage(photo_image)
+
+# Create a label with the image as its content
+photo_label = tk.Label(root, image=photo_image)
+photo_label.grid(row=4, column=1, padx=20, pady=20, rowspan=2)
 
 # Create listbox for cart
 cart_listbox = tk.Listbox(root, height=10, width=50,highlightthickness=4,highlightcolor="black",borderwidth=5)
